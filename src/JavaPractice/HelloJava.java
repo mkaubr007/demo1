7cc68358afd1e4b44f60e935fc2bc4d07547ebe7
mkaubr007 <mkaubr007@gmail.com>
2021-08-23 14:23:40 +0530

    new feature is added

--- src/JavaPractice/HelloJava.java
+++ src/JavaPractice/HelloJava.java
@@ -4,6 +4,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Welcome");
         System.out.println("To Java");
-        System.out.println("World")
+        System.out.println("World");
     }
 }
